504787bc69261935bac3f4b3d4badc77810fc9a6
seonrizee <seonrizee@gmail.com>
2025-09-16 20:38:48 +0900

    refactor(lv2-9): @Auth 을 @AuthenticationPrincipal 로 교체

--- src/main/java/org/example/expert/domain/user/controller/UserController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserController.java
@@ -1,12 +1,12 @@
 package org.example.expert.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.user.dto.request.UserChangePasswordRequest;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -25,7 +25,7 @@ public class UserController {
     }
 
     @PutMapping("/users")
-    public void changePassword(@Auth AuthUser authUser,
+    public void changePassword(@AuthenticationPrincipal AuthUser authUser,
                                @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
         userService.changePassword(authUser.id(), userChangePasswordRequest);
     }
