98f7cb3f7715fe212a4eaafa86147ac7bf8ab437
seonrizee <seonrizee@gmail.com>
2025-09-17 22:06:22 +0900

    feat(lv3-10): QueryDSL을 이용한 검색 기능 정의 및 로직 구현
    - QTodo 정적 임포트
    - @QueryProjection을 사용하여 컴파일 시점 타입 및 파라미터 개수 체크
    - 카티전 곱 문제 방지를 위한 서브쿼리 생성 manager, comment 카운트 활용
    - BooleanExpression 및 동적 조인 메서드를 활용하여 동적 쿼리 작성

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryCustom.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryCustom.java
@@ -1,9 +1,19 @@
 package org.example.expert.domain.todo.repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
+import org.example.expert.domain.todo.dto.TodoSearchProjection;
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface TodoRepositoryCustom {
 
     Optional<Todo> findByIdWithUser(Long todoId);
+
+    Page<TodoSearchProjection> findAllBySearch(String titleKeyword,
+                                               LocalDate startDate,
+                                               LocalDate endDate,
+                                               String nicknameKeyword,
+                                               Pageable pageable);
 }
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryCustomImpl.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryCustomImpl.java
@@ -1,10 +1,23 @@
 package org.example.expert.domain.todo.repository;
 
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+import static org.springframework.util.StringUtils.hasText;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.todo.entity.QTodo;
+import org.example.expert.domain.manager.entity.QManager;
+import org.example.expert.domain.todo.dto.QTodoSearchProjection;
+import org.example.expert.domain.todo.dto.TodoSearchProjection;
 import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.QUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 
 @RequiredArgsConstructor
@@ -14,9 +27,77 @@ public class TodoRepositoryCustomImpl implements TodoRepositoryCustom {
 
     @Override
     public Optional<Todo> findByIdWithUser(Long todoId) {
-        return Optional.ofNullable(queryFactory.selectFrom(QTodo.todo)
-                .leftJoin(QTodo.todo.user).fetchJoin()
-                .where(QTodo.todo.id.eq(todoId))
+        return Optional.ofNullable(queryFactory.selectFrom(todo)
+                .leftJoin(todo.user).fetchJoin()
+                .where(todo.id.eq(todoId))
                 .fetchOne());
+    }
+
+    @Override
+    public Page<TodoSearchProjection> findAllBySearch(String titleKeyword,
+                                                      LocalDate startDate,
+                                                      LocalDate endDate,
+                                                      String nicknameKeyword,
+                                                      Pageable pageable) {
+
+        JPAQuery<TodoSearchProjection> query = queryFactory
+                .select(new QTodoSearchProjection(
+                        todo.title,
+                        todo.managers.size().intValue(),
+                        todo.comments.size().intValue()
+                ))
+                .from(todo);
+
+        applyJoinsForNickname(query, nicknameKeyword);
+
+        List<TodoSearchProjection> content = query
+                .where(
+                        titleCIC(titleKeyword),
+                        startDateGoe(startDate),
+                        endDateLoe(endDate),
+                        nicknameCic(nicknameKeyword)
+                )
+                .orderBy(todo.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(todo.id.countDistinct())
+                .from(todo);
+
+        applyJoinsForNickname(countQuery, nicknameKeyword);
+
+        countQuery.where(
+                titleCIC(titleKeyword),
+                startDateGoe(startDate),
+                endDateLoe(endDate),
+                nicknameCic(nicknameKeyword));
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+    }
+
+    private BooleanExpression titleCIC(String title) {
+        return hasText(title) ? todo.title.containsIgnoreCase(title) : null;
+    }
+
+    private BooleanExpression startDateGoe(LocalDate startDate) {
+        return startDate != null ? todo.createdAt.goe(startDate.atStartOfDay()) : null;
+    }
+
+    private BooleanExpression endDateLoe(LocalDate endDate) {
+        return endDate != null ? todo.createdAt.lt(endDate.plusDays(1).atStartOfDay()) : null;
+    }
+
+    private BooleanExpression nicknameCic(String nickname) {
+        return hasText(nickname) ? todo.managers.any().user.nickname.containsIgnoreCase(nickname) : null;
+    }
+
+    private <T> JPAQuery<T> applyJoinsForNickname(JPAQuery<T> query, String nickname) {
+        if (hasText(nickname)) {
+            query.leftJoin(todo.managers, QManager.manager)
+                    .leftJoin(QManager.manager.user, QUser.user);
+        }
+        return query;
     }
 }
