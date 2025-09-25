0729b7068d3cc21d296e5c560125a050d3f0aa27
seonrizee <seonrizee@gmail.com>
2025-09-26 01:25:01 +0900

    refactor(lv3-13): 캐시 등록을 위한 CacheManager를 CaffeineCacheManager로 교체

--- src/main/java/org/example/expert/config/CacheConfig.java
+++ src/main/java/org/example/expert/config/CacheConfig.java
@@ -1,14 +1,10 @@
 package org.example.expert.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCache;
-import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,20 +14,19 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        List<CaffeineCache> caches = Arrays.stream(CacheType.values())
-                .map(cacheType -> new CaffeineCache(
-                                cacheType.getCacheName(),
-                                Caffeine.newBuilder()
-                                        .recordStats() // 캐시 사용 통계 기록
-                                        .expireAfterAccess(cacheType.getExpiredAfterAccess(), TimeUnit.SECONDS)
-                                        .maximumSize(cacheType.getMaximumSize())
-                                        .build()
-                        )
-                )
-                .collect(Collectors.toList());
 
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-        cacheManager.setCaches(caches);
+        // 학습을 위해 사용했던 기본 SimpleCacheManager 대신 CaffeineCacheManager 사용
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+        for (CacheType cacheType : CacheType.values()) {
+            cacheManager.registerCustomCache(cacheType.getCacheName(),
+                    Caffeine.newBuilder()
+                            .recordStats()
+                            .expireAfterAccess(cacheType.getExpiredAfterAccess(), TimeUnit.SECONDS)
+                            .maximumSize(cacheType.getMaximumSize())
+                            .build());
+        }
+
         return cacheManager;
     }
 }
